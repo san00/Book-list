cc742b46c70ce076892dffb8ef609c275f816c7b
sanscomp <sanscomp@anslems-iMac.lan>
2019-11-22 17:38:38 +0000

    Add text for missing values & add comma between multiple values

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
       return <article className="book__container" key={index}>
         <img className="book__image" src={book.volumeInfo.imageLinks.thumbnail} alt="front cover of book"></img>
         <h2 className="book__title">{book.volumeInfo.title}</h2>
-        <p className="book__author">Author: {book.volumeInfo.authors}</p>
-        <p className="book__publisher">Publisher: {book.volumeInfo.publisher}</p>
+        <p className="book__author">Author: {book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : "Info unavailable"}</p>
+        <p className="book__publisher">Publisher: {book.volumeInfo.publisher ? book.volumeInfo.publisher : "Info unavailable"}</p>
         <button className="book__button" onClick={() => addFavourites()}>Add to favourites</button>
       </article>
     })) : "Unfortunately an error has occurred, books are currently unavailable"
