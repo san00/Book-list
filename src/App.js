647a71d80c47a9e87529d5b22adefe06fa3e4745
sanscomp <sanscomp@anslems-iMac.lan>
2020-04-18 15:06:54 +0100

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 import FetchData from './components/FetchData'
+import Header from './components/Header'
 import './App.scss';
 
 function App() {
   return (
-      <main role="main" className="app">
-      <header className="app_header">
-        <h1 className="app_header-title">Books list</h1>
-      </header>
+    <main >
+      <Header />
       <FetchData />
     </main >
   );
