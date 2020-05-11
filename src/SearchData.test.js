b50136d4c8a9eca1c0576788e23de0849a2a958c
sanscomp <sanscomp@anslems-iMac.lan>
2020-05-11 20:35:08 +0100

    Test form submits

--- src/SearchData.test.js
+++ src/SearchData.test.js
@@ -23,17 +23,34 @@ test('SearchData renders without error', () => {
 })
 
 describe('Capture events entered into form', () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
     test('State updates with the value of search box on change', () => {
         const mockOnChange = jest.fn()
-        const mockHandleSubmit =jest.fn()
+        const mockHandleSubmit = jest.fn()
 
-        const wrapper = setup({handleChange:mockOnChange, handleSubmit:mockHandleSubmit})
+        const wrapper = setup({ handleChange: mockOnChange, handleSubmit: mockHandleSubmit })
         const searchInput = findByTestAttr(wrapper, 'component-searchData-input')
 
-        const mockEvent = {target: { value: 'cars' } }
+        const mockEvent = { target: { value: 'cars' } }
         searchInput.simulate('change', mockEvent)
 
         expect(mockOnChange).toHaveBeenCalledWith(mockEvent)
     })
- 
+
+    it('Submits the form to the API', () => {
+        const mockHandleSubmit = jest.fn()
+        const mockOnChange = jest.fn()
+
+        const wrapper = setup({ handleSubmit: mockHandleSubmit, handleChange: mockOnChange })
+        const submitForm = findByTestAttr(wrapper, 'form-submit')
+
+        const mockSubmit = { target: { value: 'books' } }
+        submitForm.simulate('submit', mockSubmit)
+
+        expect(mockHandleSubmit).toHaveBeenCalledWith(mockSubmit)
+    })
 })
+
--- src/components/SearchData.js
+++ src/components/SearchData.js
@@ -4,7 +4,7 @@ function SearchData({ handleChange, handleSubmit }) {
 
     return (
         <section data-test='component-searchData'>
-            <form className='form' onSubmit={handleSubmit}>
+            <form data-test='form-submit' className='form' onSubmit={handleSubmit}>
                 <label className='form__label' htmlFor='searchbox'>
                     <input data-test='component-searchData-input' className='form__searchbox'
                         id='searchbox' type='text'
