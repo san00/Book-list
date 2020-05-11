6148bd1c5846d3e518a386a4eb85dc0ac2ee669e
sanscomp <sanscomp@anslems-iMac.lan>
2020-05-11 21:19:42 +0100

    Remove code merged in error

--- src/actions/hookActions.test.js
+++ src/actions/hookActions.test.js
@@ -31,26 +31,4 @@ describe('moxios test', () => {
         // See whether mock was run with correct argument
         expect(mockData).toHaveBeenCalledWith(bookData)
     })
-
-    // test('Error is returned if no api response', async () => {
-    //     const error = 'Error'
-
-    //     moxios.wait(() => {
-    //         const request = moxios.requests.mostRecent()
-    //         request.respondWith({
-    //             status: 501,
-    //             response: error,
-    //         })
-    //     })
-
-    //     // Create mock for callback arg
-    //     const mockError = jest.fn()
-
-    //     await mockAxiosCall(mockError)
-
-    //     // See whether mock was run with correct argument
-    //     expect(mockError).toHaveBeenCalledWith(error)
-    // })
-
-    
 })
